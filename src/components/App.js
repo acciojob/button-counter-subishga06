cd82a035929aaa3b7a426f8f980482f7fe8fb1e4
subishga06 <subishgavs@gmail.com>
2026-01-04 18:33:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ import './../styles/App.css';
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Click Me
+      </button>
     </div>
   )
 }
